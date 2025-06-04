4a0a0e7d3a3143b508d71beaf529d50ac7f1a7f4
Katelyn <94091546+KatelynTheStargazer@users.noreply.github.com>
2025-06-04 13:46:03 -0400

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,13 +1,18 @@
-MODDIR=${0%/*}
+#!/system/bin/sh
 
-sleep 8
+while [ "$(getprop init.svc.vendor.sensors-hal-multihal)" != "running" ]; do
+  sleep 0.1
+done
 
 ANDROID_VERSION=$(getprop ro.build.version.release)
-
-if [ "$ANDROID_VERSION" = "13" ] || [ "$ANDROID_VERSION" = "14" ] || [ "$ANDROID_VERSION" = "15" ] || [ "$ANDROID_VERSION" = "16" ]; then
+case "$ANDROID_VERSION" in
+  13|14|15|16)
     service call sensor_privacy 9 i32 1
-elif [ "$ANDROID_VERSION" = "12" ]; then
+    ;;
+  12)
     service call sensor_privacy 8 i32 1
-elif [ "$ANDROID_VERSION" = "11" ] || [ "$ANDROID_VERSION" = "10" ]; then
+    ;;
+  10|11)
     service call sensor_privacy 4 i32 1
-fi
+    ;;
+esac
