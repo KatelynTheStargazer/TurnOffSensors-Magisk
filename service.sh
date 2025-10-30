f5a415ac5d4e4ee8af4dc1569f60838c788c2a01
adalyn <Lutetium71RXTE@proton.me>
2025-10-30 04:23:00 -0400

    first commit

---
+++ service.sh
@@ -0,0 +1,18 @@
+#!/system/bin/sh
+
+while ! service list | grep -q sensor_privacy; do
+  sleep 0.1
+done
+
+ANDROID_VERSION=$(getprop ro.build.version.release)
+case "$ANDROID_VERSION" in
+  13|14|15|16)
+    service call sensor_privacy 9 i32 1
+    ;;
+  12)
+    service call sensor_privacy 8 i32 1
+    ;;
+  10|11)
+    service call sensor_privacy 4 i32 1
+    ;;
+esac
